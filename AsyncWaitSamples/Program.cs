8dcb91ab7fd61f3444d5481f423b05a913719cdb
erich brunner <erich.brunner@live.at>
2016-04-26 10:30:52 +0200

    Add project files.

---
+++ AsyncWaitSamples/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncWaitSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+
+            // Fire and Forget the 'async' method ComputeSomeAsync just for fun
+            // Note: Any exception that occures inside method 'ComputeSomeAsync' can only be caught in the 'UnhandledException' hander of the current AppDomain.
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            var computeTask = ComputeSomeAsync(async (number) =>
+            {
+                int doubled = number * 2;
+                await Task.Delay(500 * doubled); // simulate external I/O operation
+                System.Diagnostics.Debug.WriteLine($"In continuation of calculateFunc for number:{doubled} in thread:{Thread.CurrentThread.ManagedThreadId}");
+                return doubled;
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            System.Diagnostics.Debug.WriteLine($"Main Thread other work finished. Waiting for ComputeSomeAsync to complete in thread:{Thread.CurrentThread.ManagedThreadId}");
+
+            while (!computeTask.IsCompleted)
+            {
+                System.Diagnostics.Debug.WriteLine("Do some main work.");
+                Thread.Sleep(10);
+            }
+            
+            System.Diagnostics.Debug.WriteLine($"Main Thread other work finished");
+            
+            Console.ReadLine();
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine(e.ExceptionObject);
+        }
+
+        private static async Task ComputeSomeAsync(Func<int, Task<int>> calculateFunc)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                int result = await calculateFunc(i);
+                System.Diagnostics.Debug.WriteLine($"In continuation of ComputeSomeAsync for result:{ result} in thread:{Thread.CurrentThread.ManagedThreadId}");
+            }
+
+            System.Diagnostics.Debug.WriteLine("ComputeSomeAsync finished");
+        }
+
+    }
+}
---
+++ PrintAnswerToLife/PrintAnswerToLife/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAnswerToLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var printTask = PrintAnswerToLife();
+
+            printTask.Wait();
+            Log("main thread: PrintAnswerToLife finished");
+
+            // The same as above without the use of the compiler service of the 'async' and 'await' syntax sugar.
+            var printTaskVerbose = PrintAnswerToLifeVerbose();
+            printTaskVerbose.Wait();
+
+            Log("main thread: PrintAnswerToLifeVerbose finished");
+
+            Console.ReadLine();
+        }
+
+        private static async Task PrintAnswerToLife()
+        {
+            await Task.Delay(5000);
+            ContinueHere();
+        }
+
+        private static Task PrintAnswerToLifeVerbose()
+        {
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+
+            TaskAwaiter awaiter = Task.Delay(20000).GetAwaiter();
+            awaiter.OnCompleted(() =>
+            {
+                try
+                {
+                    // Ends the awaited task
+                    awaiter.GetResult(); // Re-throw any eoccured exception
+
+                    ContinueHere();
+
+                    tcs.SetResult(null); // Return type is 'Task'. So nothing to set as result.
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            });
+
+            return tcs.Task;
+        }
+
+        private static void ContinueHere()
+        {
+            int answer = 21 * 2;
+            Log(answer.ToString());
+        }
+
+        private static void Log(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+            Console.WriteLine(message);
+        }
+    }
+}
